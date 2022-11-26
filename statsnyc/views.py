6654976d2a207682ff5db8b4ca24271a631142e4
Eren Akbas <ea2361@nyu.edu>
2022-11-26 12:55:18 -0500

    nov 26 update

--- statsnyc/views.py
+++ statsnyc/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 #importing tables
-from .models import Airq, Greeninfra
+from .models import Airq, Greeninfra, Energy
 #connection for raw sql queries to the db
 from django.db import connection, transaction
 from django.db.models import F
@@ -10,10 +10,18 @@ from .place_to_geocode import geocode
 
 #getAirq accepts an http request with a place GET parameter 
 #returns an http response with all the air quality metrics for it
-def getAirq(request):
-	place = request.GET.get('place', '')
-	result = Airq.objects.filter(geo_place = place)
+def getAirq(lng, lat):
+# return first AQI from database using longitude, latitide
+	for obj in Airq.objects.all():
+		if (obj.latitude_sw <= lat <= obj.latitude_ne) and \
+			(obj.longitude_sw <= lng <= obj.longitude_ne):
+			return obj.aqi
+	return HttpResponse("ERROR: 404 Not Found.")
 
+
+	# place = request.GET.get('place', '')
+	# result = Airq.objects.filter(geo_place = place)
+	
 	#for obj in Airq.objects.all():
 	#	pl = obj.geo_place
 	#
@@ -24,10 +32,20 @@ def getAirq(request):
 	#	obj.longitude_sw= gcode[1][1]	
 	#	obj.save()
 
-	
+	#return HttpResponse(result.values())
 
+#return averge energy score from database using longitude, latitude
+def getEnergy(lng, lat):
+	# lng = request.GET.get("lng", None)
+	# lat = request.GET.get("lat", None)
+	values = []
+	for obj in Energy.objects.all():
+		if isinstance(obj.score, int):
+			if (abs(lng - obj.lng) < 0.1 and abs(lat - obj.lng) < 0.1): # area threshold (0.1) for consideration
+				values.append(obj.score)
+	avg = avg(values)
+	return avg
 
-	return HttpResponse(result.values())
 
 def calcAQI(request):
 	for obj in Airq.objects.all():
@@ -36,12 +54,30 @@ def calcAQI(request):
 		obj.save()
 
 
-
 #getGreeninfra accepts an http request with longitude and latitude parameters
 #returns an http response with the green infrastructure metrics for the coordinates
-def getGreeninfra(request):
-	lat = request.GET.get('lat','')
-	lng = request.GET.get('lng', '')
-	result = Greeninfra.objects.filter(latitude = lat, longitude = lng)
+def getGreeninfra(lng, lat):
+	# lat = request.GET.get('lat','')
+	# lng = request.GET.get('lng', '')
+	# result = Greeninfra.objects.filter(latitude = lat, longitude = lng)
 
-	return HttpResponse(result.values())
+	# return HttpResponse(result.values())
+	
+#return averge greeninfra value from database using longitude, latitude
+	values = []
+	for obj in Greeninfra.objects.all():
+		if (abs(lng - obj.lng) < 0.1 and abs(lat - obj.lng) < 0.1): # area threshold (0.1) for consideration
+			values.append(obj.area)
+	avg = avg(values)
+	return avg
+
+# calculate final greenscore using geolocation
+def getGreenScore(request):
+	lng = request.GET.get("lng", None)
+	lat = request.GET.get("lat", None)
+	assert (None not in [lng, lat])
+	energy = getEnergy(lng, lat) # get energy
+	airq = getAirq(lng, lat)
+	infra = getGreeninfra(lng, lat)
+	greenscore = (energy + airq + infra) // 3
+	return HttpResponse() 
