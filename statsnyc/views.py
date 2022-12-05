171f19ba197dde105717ff5da9bb9bb2952a3cb0
Eren Akbas <ea2361@nyu.edu>
2022-12-04 22:15:31 -0500

    fixed function for greeninfra and energy

--- statsnyc/views.py
+++ statsnyc/views.py
@@ -8,6 +8,8 @@ from django.db.models import F
 from django.http import HttpResponse
 from .place_to_geocode import geocode
 
+import math
+
 #getAirq accepts an http request with a place GET parameter 
 #returns an http response with all the air quality metrics for it
 def getAirq(lng, lat):
@@ -15,36 +17,24 @@ def getAirq(lng, lat):
 	for obj in Airq.objects.all():
 		if (obj.latitude_sw <= lat <= obj.latitude_ne) and \
 			(obj.longitude_sw <= lng <= obj.longitude_ne):
-			return obj.aqi
+			aqi = obj.aqi
+			return HttpResponse(aqi)
 	return HttpResponse("ERROR: 404 Not Found.")
 
-
-	# place = request.GET.get('place', '')
-	# result = Airq.objects.filter(geo_place = place)
-	
-	#for obj in Airq.objects.all():
-	#	pl = obj.geo_place
-	#
-	#	gcode = geocode(pl)
-	#	obj.latitude_ne = gcode[0][0]
-	#	obj.longitude_ne = gcode[0][1]
-	#	obj.latitude_sw = gcode[1][0]
-	#	obj.longitude_sw= gcode[1][1]	
-	#	obj.save()
-
-	#return HttpResponse(result.values())
-
 #return averge energy score from database using longitude, latitude
-def getEnergy(lng, lat):
-	# lng = request.GET.get("lng", None)
-	# lat = request.GET.get("lat", None)
+def getEnergy(request):
+	def dist_away(lngA, latA, lngB, latB):
+		a = (math.sin((latB - latA)/2))**2 + math.cos(latA) * math.cos(latB) * (math.sin((lngB - lngA)/2))**2
+		return 2 * 3956 * math.asin(a)
+
+	lng = request.GET.get("lng", None)
+	lat = request.GET.get("lat", None)
 	values = []
 	for obj in Energy.objects.all():
-		if isinstance(obj.score, int):
-			if (abs(lng - obj.lng) < 0.1 and abs(lat - obj.lng) < 0.1): # area threshold (0.1) for consideration
-				values.append(obj.score)
-	avg = avg(values)
-	return avg
+		if dist_away(lng, lat, obj.lng, obj.lat) < 0.5: # area threshold (0.1) for consideration
+			values.append(obj.score)
+	energy = sum(values) // len(values)
+	return HttpResponse(energy)
 
 
 def calcAQI(request):
@@ -56,20 +46,18 @@ def calcAQI(request):
 
 #getGreeninfra accepts an http request with longitude and latitude parameters
 #returns an http response with the green infrastructure metrics for the coordinates
-def getGreeninfra(lng, lat):
-	# lat = request.GET.get('lat','')
-	# lng = request.GET.get('lng', '')
-	# result = Greeninfra.objects.filter(latitude = lat, longitude = lng)
-
-	# return HttpResponse(result.values())
+def getGreeninfra(request):
+	lng = request.GET.get("lng", None)
+	lat = request.GET.get("lat", None)
+	def dist_away(lngA, latA, lngB, latB):
+		a = (math.sin((latB - latA)/2))**2 + math.cos(latA) * math.cos(latB) * (math.sin((lngB - lngA)/2))**2
+		return 2 * 3956 * math.asin(a)
 	
-#return averge greeninfra value from database using longitude, latitude
-	values = []
+	greenarea_total = 0
 	for obj in Greeninfra.objects.all():
-		if (abs(lng - obj.lng) < 0.1 and abs(lat - obj.lng) < 0.1): # area threshold (0.1) for consideration
-			values.append(obj.area)
-	avg = avg(values)
-	return avg
+		if dist_away(lng, lat, obj.lng, obj.lat) < 0.5:
+			greenarea_total += obj.area
+	return HttpResponse(greenarea_total)
 
 # calculate final greenscore using geolocation
 def getGreenScore(request):
