72bb540d8db8cc23741740338d37bf30065d149c
Eren Akbas <ea2361@nyu.edu>
2022-12-05 00:18:16 -0500

    fixed distance function

--- statsnyc/views.py
+++ statsnyc/views.py
@@ -8,7 +8,19 @@ from django.db.models import F
 from django.http import HttpResponse
 from .place_to_geocode import geocode
 
-import math
+import numpy as np
+
+def distance(lat1, lon1, lat2, lon2, r=3956):
+    	# Convert degrees to radians
+		coordinates = lat1, lon1, lat2, lon2
+    	# radians(c) is same as c*pi/180
+		phi1, lambda1, phi2, lambda2 = [np.radians(c) for c in coordinates]  
+    
+    	# Apply the haversine formula
+		a = (np.square(np.sin((phi2-phi1)/2)) + np.cos(phi1) * np.cos(phi2) * np.square(np.sin((lambda2-lambda1)/2)))
+		d = 2*r*np.arcsin(np.sqrt(a))
+		print(d)
+		return d
 
 #getAirq accepts an http request with a place GET parameter 
 #returns an http response with all the air quality metrics for it
@@ -23,15 +35,12 @@ def getAirq(lng, lat):
 
 #return averge energy score from database using longitude, latitude
 def getEnergy(request):
-	def dist_away(lngA, latA, lngB, latB):
-		a = (math.sin((latB - latA)/2))**2 + math.cos(latA) * math.cos(latB) * (math.sin((lngB - lngA)/2))**2
-		return 2 * 3956 * math.asin(a)
 
 	lng = request.GET.get("lng", None)
 	lat = request.GET.get("lat", None)
 	values = []
 	for obj in Energy.objects.all():
-		if dist_away(lng, lat, obj.lng, obj.lat) < 0.5: # area threshold (0.1) for consideration
+		if distance(float(lat), float(lng), float(obj.lat), float(obj.lng)) < 0.5: # area threshold (0.1) for consideration
 			values.append(obj.score)
 	energy = sum(values) // len(values)
 	return HttpResponse(energy)
@@ -49,16 +58,16 @@ def calcAQI(request):
 def getGreeninfra(request):
 	lng = request.GET.get("lng", None)
 	lat = request.GET.get("lat", None)
-	def dist_away(lngA, latA, lngB, latB):
-		a = (math.sin((latB - latA)/2))**2 + math.cos(latA) * math.cos(latB) * (math.sin((lngB - lngA)/2))**2
-		return 2 * 3956 * math.asin(a)
-	
+	# def dist_away(lngA, latA, lngB, latB):
+	# 	a = ((math.sin((latB - latA)/2))**2) + math.cos(latA) * math.cos(latB) * ((math.sin((lngB - lngA)/2))**2)
+	# 	return 2 * 3956 * math.asin(math.sqrt(a))
+
 	greenarea_total = 0
 
-	
+	# dist_away(float(lng), float(lat), float(obj.longitude), float(obj.latitude)) 
 	for obj in Greeninfra.objects.all():
-		if dist_away(float(lng), float(lat), float(obj.longitude), float(obj.latitude)) < 0.5:
-			greenarea_total += obj.area
+		if distance(float(lat), float(lng), float(obj.latitude), float(obj.longitude)) < 0.5:
+			greenarea_total += float(obj.area)
 	return HttpResponse(greenarea_total)
 
 # calculate final greenscore using geolocation
